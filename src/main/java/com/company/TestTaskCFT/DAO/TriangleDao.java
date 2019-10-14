d48fa2aaf1b29fcc4483b6689fabcb72786588f5
AltaVista333 <nongames333@gmail.com>
2019-10-14 23:25:31 +0300

    add safe method for find all triangles

--- src/main/java/com/company/TestTaskCFT/DAO/TriangleDao.java
+++ src/main/java/com/company/TestTaskCFT/DAO/TriangleDao.java
@@ -5,8 +5,10 @@ import com.company.TestTaskCFT.Model.Triangle;
 import com.company.TestTaskCFT.Service.Validator;
 import com.company.TestTaskCFT.Utility.Utility;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -39,6 +41,22 @@ public class TriangleDao {
         return getAllTrianglesFromDataSource()
                 .filter(Triangle::isTriangleIsosceles)
                 .collect(Utility.collector(Triangle::compareTo));
+    }
+
+    public List<Triangle> getAllMaxAreaIsoscelesTrianglesSafe() {
+        List<Triangle> list = new ArrayList<>();
+        Optional<Triangle> optionalTriangle = getMaxAreaIsoscelesTriangle();
+        optionalTriangle.ifPresent(
+                triangle -> list.addAll(getAllMaxAreaIsoscelesTriangles(triangle.getArea()))
+        );
+        return list;
+    }
+
+    public List<Triangle> getAllMaxAreaIsoscelesTriangles(double triangleArea){
+        return getAllTrianglesFromDataSource()
+                .filter(Triangle::isTriangleIsosceles)
+                .filter(triangle -> triangle.getArea() == triangleArea)
+                .collect(Collectors.toList());
     }
 
     public void saveTriangle(Triangle triangle) {
